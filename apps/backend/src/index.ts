a6420d13746bbc01a07c52c91bb268d012dec89a
Avashneupane9857 <juisch20220011252@gmail.com>
2026-03-02 21:03:57 +0545

    be err

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -37,7 +37,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"],
   })
 );
 
